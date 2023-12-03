47ef3a58d2d0e21997ef92e88da1032f4a0da033
Felipe Pinheiro <santospinheiro627@gmail.com>
2023-12-03 20:33:58 -0300

    test livewire

---
+++ app/Livewire/Animes/AnimeUpdate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Animes;
+
+use Livewire\Component;
+
+class AnimeUpdate extends Component
+{
+    public function render()
+    {
+        return view('livewire.animes.anime-update');
+    }
+}
