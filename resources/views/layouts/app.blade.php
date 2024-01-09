754a3023c007b523b92dbe61d0b3495d06ff92da
Felipe Pinheiro <santospinheiro627@gmail.com>
2024-01-09 20:46:45 -0300

    add tailwind css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
+
+        @livewireStyles
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
+
     <body>
         {{ $slot }}
     </body>
+    
+    @livewireScripts
 </html>
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,3 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <h1>Felipew</h1>
 </div>
